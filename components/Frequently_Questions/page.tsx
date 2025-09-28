356b0c1c6170b2c03a74c54e6a715ae142e4293d
Tofazzal26 <mdtofazzalislam6789@gmail.com>
2025-09-29 02:07:23 +0600

    Frequently asked questions desktop and laptop and phone device responsibe finish

--- components/Frequently_Questions/page.tsx
+++ components/Frequently_Questions/page.tsx
@@ -26,31 +26,31 @@ const Frequently_Questions: React.FC = () => {
   ];
 
   return (
-    <div className="mt-20">
+    <div className="lg:mt-20 md:mt-14 mt-10">
       <div className="">
         <div className="">
-          <h2 className="text-5xl font-bold text-center">
+          <h2 className="lg:text-5xl md:text-[32px] text-2xl font-bold text-center">
             Frequently asked questions
           </h2>
-          <div className="max-w-[920px] mx-auto mt-[120px] space-y-4">
+          <div className="lg:max-w-[920px] md:max-w-[688px] mx-auto lg:mt-[120px] md:mt-[40px] mt-8 space-y-4 lg:px-0 md:px-0 px-6">
             {faqs.map((item, index) => (
               <div key={index} className="collapse collapse-arrow">
                 <input type="checkbox" defaultChecked={index === 0} />
                 <div
-                  className={`collapse-title text-[28px] font-semibold leading-[40px] px-0 ${
+                  className={`collapse-title lg:text-[28px] md:text-xl text-base font-semibold leading-[40px] px-0 ${
                     index == 2
-                      ? "border-t-[1px] border-[#D4DBE0] rounded-none py-8"
+                      ? "border-t-[1px] border-[#D4DBE0] rounded-none lg:py-8 md:py-6 py-7"
                       : index === 1
-                      ? "border-t-[1px] border-[#D4DBE0] pt-7 rounded-none"
+                      ? "border-t-[1px] border-[#D4DBE0] lg:pt-7 md:pt-6 pt-6 rounded-none"
                       : index === 3
-                      ? "border-t-[1px] border-[#D4DBE0] rounded-none pt-7"
+                      ? "border-t-[1px] border-[#D4DBE0] rounded-none lg:pt-7 md:pt-8 pt-7"
                       : ""
                   }`}
                 >
                   {item.question}
                 </div>
                 <div className="collapse-content px-0">
-                  <p className="text-lg font-normal leading-[30px] text-[#2a2f32]">
+                  <p className="lg:text-lg md:text-sm text-sm font-normal leading-[30px] text-[#2a2f32]">
                     {item.answer}
                   </p>
                 </div>
