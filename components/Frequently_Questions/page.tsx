16db190ba2f7b69acb5308648d493ac2cb709bca
Tofazzal26 <mdtofazzalislam6789@gmail.com>
2025-09-28 20:15:43 +0600

    business logo all file setup finish

---
+++ components/Frequently_Questions/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Frequently_Questions: React.FC = () => {
+  return (
+    <div>
+      <h2>Frequently_Questions</h2>
+    </div>
+  );
+};
+
+export default Frequently_Questions;
